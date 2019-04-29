8922e20304f450d5eb951eb6e487fef8c7cdd331
AldenMcGonagle <aldenmcgonagle@seattleacademy.org>
2019-04-29 09:15:24 -0700

    Add index.html fo serving tests

--- server.js
+++ server.js
@@ -6,35 +6,24 @@ const port = 1201
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(express.static('public'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 var gsjson = require('google-spreadsheet-to-json');
 
-// gsjson({
-//         spreadsheetId: '1ynOQ2h6FUxHNkxHB2Ca6qPeyHxQo8qjJkk7U5T6tKTU',
-//         // other options...
-//     })
-//     .then(function(result) {
-//         console.log(result.length);
-//         console.log(result);
-//     })
-//     .catch(function(err) {
-//         console.log(err.message);
-//         console.log(err.stack);
-//     });
-
 app.get('/json', (req, res) => {
     gsjson({
-        spreadsheetId: '1ynOQ2h6FUxHNkxHB2Ca6qPeyHxQo8qjJkk7U5T6tKTU',
-        // other options...
-    })
-    .then(function(result) {
-        console.log(result.length);
-        console.log(result);
-        res.send(result)
-    })
-    .catch(function(err) {
-        console.log(err.message);
-        console.log(err.stack);
-    });
+            spreadsheetId: '1ynOQ2h6FUxHNkxHB2Ca6qPeyHxQo8qjJkk7U5T6tKTU',
+            // other options...
+        })
+        .then(function(result) {
+            console.log(result.length);
+            console.log(result);
+            res.send(result)
+        })
+        .catch(function(err) {
+            console.log(err.message);
+            console.log(err.stack);
+        });
 })
